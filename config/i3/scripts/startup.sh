bc487e0fd157b9606062a33960baf1549cf69f9a
me-js-bro <me.js.bro@gmail.com>
2024-07-14 23:12:02 +0600

    fixed some bugs

--- config/i3/scripts/startup.sh
+++ config/i3/scripts/startup.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-"$HOME/.config/polybar/launch.sh" #polybar
-feh --bg-scale "$HOME/.config/i3/.cache/current.png" #wallpaper
-"$HOME/.config/i3/scripts/pywal.sh"
-"$HOME/.config/i3/scripts/polkit.sh"
-
 if [[ -d "/usr/share/openbangla-keyboard" ]]; then
     fcitx5 & #fcitx5 if openbangla keyboard is installed
 fi
-
-picom
-greenclip daemon
